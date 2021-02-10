81360da7ec6aa7c0a5da960ad0da11adb5d4d870
ggomjae <ggomjae@naver.com>
2021-02-11 01:42:59 +0900

    gradle update 및 Homecontroller 임시 테스트

--- module-batch/src/main/java/com/ggomjae/com/modulebatch/ModuleBatchApplication.java
+++ module-batch/src/main/java/com/ggomjae/com/modulebatch/ModuleBatchApplication.java
@@ -3,12 +3,15 @@ package com.ggomjae.com.modulebatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableBatchProcessing  //배치기능 활성화
+@EnableScheduling       // 스케줄러 기능을 사용할수 있게 활성화
+@EnableBatchProcessing  // 배치기능 활성화
 @SpringBootApplication
 public class ModuleBatchApplication {
 
     public static void main(String[] args) {
+        System.out.println("-------------batch-----------------");
         SpringApplication.run(ModuleBatchApplication.class, args);
     }
 
--- statistics/src/main/java/com/ggomjae/com/statistics/controller/HomeController.java
+++ statistics/src/main/java/com/ggomjae/com/statistics/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.ggomjae.com.statistics.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HomeController {
 
     @GetMapping("/home")
     public String homeMethod(){
+
         return "GGOMJAE";
     }
 }
