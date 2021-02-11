1e5a3557de1828321904b5757c5595339eb76059
ggomjae <ggomjae@naver.com>
2021-02-11 14:27:45 +0900

    module-commons 구현 및 statistics에서 DB 연동 셋팅

--- statistics/src/main/java/com/ggomjae/com/statistics/StatisticsApplication.java
+++ statistics/src/main/java/com/ggomjae/com/statistics/StatisticsApplication.java
@@ -2,8 +2,12 @@ package com.ggomjae.com.statistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.ggomjae.com.db"})
+@EnableJpaRepositories(basePackages = {"com.ggomjae.com.db"})
 public class StatisticsApplication {
 
     public static void main(String[] args) {
